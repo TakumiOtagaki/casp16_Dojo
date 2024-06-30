3dee50001492b870e5f7ac03d88a42df6525efd7
Takumi Otagaki <otagaki-takumi@g.ecc.u-tokyo.ac.jp>
2024-07-01 00:16:05 +0900

    box について修正

--- utils/multimer_estimation.py
+++ utils/multimer_estimation.py
@@ -12,11 +12,11 @@ from Bio.PDB import Model
 from Bio.PDB import Structure
 import numpy as np
 import sys
-torch.autograd.set_detect_anomaly(True)
+# torch.autograd.set_detect_anomaly(True)
 
 
 class RNAOptimizationModel(nn.Module):
-    def __init__(self, n_units, pdb_file_path, spacing=300.0, device='cpu', alpha=1.0, lambda1=10, lambda2=10, delta=1.0, sigma=1.0, compression=True):
+    def __init__(self, n_units, pdb_file_path, spacing=75.0, device='cpu', alpha=1.0, lambda1=1, lambda2=1, delta=0.1, sigma=1.0, compression=True):
         super(RNAOptimizationModel, self).__init__()
         self.n_units = n_units
         self.device = device
@@ -36,8 +36,17 @@ class RNAOptimizationModel(nn.Module):
         self.positions = nn.Parameter(positions)
 
         # Initialize Euler angles for rotation matrices
-        self.euler_angles = nn.Parameter(
-            torch.randn(n_units, 3, device=self.device))
+        # self.euler_angles = nn.Parameter(
+        #     torch.randn(n_units, 3, device=self.device))
+        # 空間上に。対象に配置する：角度は 2pi / n_units ずつずらす
+        self.eulerAngles = torch.zeros(n_units, 3, device=device)
+        angle = 2 * math.pi / n_units
+        for i in range(n_units):
+            self.eulerAngles[i, 0] = i * angle
+            self.eulerAngles[i, 1] = torch.randn(1, device=device) * 0.1
+            self.eulerAngles[i, 2] = torch.randn(1, device=device) * 0.1
+        self.euler_angles = nn.Parameter(self.eulerAngles)
+            
 
         # Load coordinates from a PDB file
         self.parser = PDBParser(QUIET=True)
@@ -72,7 +81,7 @@ class RNAOptimizationModel(nn.Module):
                         self.coordinates.append(c1_coord)
         self.coordinates = torch.stack(self.coordinates).view(-1, 3).float()
 
-    def compress_coordinates(self, threshold=0.70):
+    def compress_coordinates(self, threshold=0.9):
         initial_count = len(self.coordinates)
         compressed_coords = []
         i = 0
@@ -95,12 +104,10 @@ class RNAOptimizationModel(nn.Module):
         print(
             f"Compressed from {initial_count} to {len(self.coordinates)} atoms")
 
-    def calculate_boxes(self):
-        print("hi")
-        min_coords = torch.min(self.coordinates, dim=0).values
-        max_coords = torch.max(self.coordinates, dim=0).values
-        self.box = torch.stack([min_coords, max_coords], dim=0)  # [2, 3]
-        print("end hi")
+    # def calculate_boxes(self):
+    #     min_coords = torch.min(self.coordinates, dim=0).values
+    #     max_coords = torch.max(self.coordinates, dim=0).values
+    #     self.box = torch.stack([min_coords, max_coords], dim=0)  # [2, 3]
 
     def forward(self):
         rotations = self.compute_rotation_matrices(self.euler_angles)
@@ -169,47 +176,48 @@ class RNAOptimizationModel(nn.Module):
 
         # L_interaction / (n * (n - 1) * m**2)
         L_interaction = L_interaction / (n * (n - 1) * m**2)
-        print("L_overlap")
-        L_overlap = self.calculate_overlap_loss(
-            positions, rotations) * self.lambda2 * 3
+        # print("L_overlap")
+        # L_overlap = (self.calculate_overlap_loss(
+        #     positions, rotations) * self.lambda2  / (n * (n - 1)))
 
         total_loss = L1 + L_center_of_mass + L_interaction + L_overlap
+        print(f"\tn{n}, L1: {L1}, L_center_of_mass: {L_center_of_mass}, L_interaction: {L_interaction}, L_overlap: {L_overlap}")
 
         return total_loss
 
-    def calculate_overlap_loss(self, positions, rotations):
-        n = positions.size(0)
-        L_overlap = torch.tensor(0.0, device=self.device)
-        boxes = []
+    # def calculate_overlap_loss(self, positions, rotations):
+    #     n = positions.size(0)
+    #     L_overlap = torch.tensor(0.0, device=self.device)
+    #     boxes = []
 
-        for i in range(n):
-            box = self.box.clone()
-            # box[0] = torch.matmul(rotations[i], box[0]) + positions[i]
-            # box[1] = torch.matmul(rotations[i], box[1]) + positions[i]
-            tmp = torch.zeros(2, 3, device=self.device)
-            tmp[0] = tmp[0] + torch.matmul(rotations[i], box[0]) + positions[i]
-            tmp[1] = tmp[1] + torch.matmul(rotations[i], box[1]) + positions[i]
-            box = box + tmp
+    #     for i in range(n):
+    #         box = self.box.clone()
+    #         # box[0] = torch.matmul(rotations[i], box[0]) + positions[i]
+    #         # box[1] = torch.matmul(rotations[i], box[1]) + positions[i]
+    #         tmp = torch.zeros(2, 3, device=self.device)
+    #         tmp[0] = tmp[0] + torch.matmul(rotations[i], box[0]) + positions[i]
+    #         tmp[1] = tmp[1] + torch.matmul(rotations[i], box[1]) + positions[i]
+    #         box = box + tmp
             
-            boxes.append(box)
+    #         boxes.append(box)
 
-        for i in range(n):
-            for j in range(i + 1, n):
-                overlap = self.box_overlap_volume(boxes[i], boxes[j])
-                L_overlap = L_overlap + overlap
+    #     for i in range(n):
+    #         for j in range(i + 1, n):
+    #             overlap = self.box_overlap_volume(boxes[i], boxes[j])
+    #             L_overlap = L_overlap + overlap
 
-        return L_overlap
+    #     return L_overlap
 
-    def box_overlap_volume(self, box1, box2):
-        min_corner1, max_corner1 = box1
-        min_corner2, max_corner2 = box2
+    # def box_overlap_volume(self, box1, box2):
+    #     min_corner1, max_corner1 = box1
+    #     min_corner2, max_corner2 = box2
 
-        overlap_min = torch.max(min_corner1, min_corner2)
-        overlap_max = torch.min(max_corner1, max_corner2)
-        overlap_dims = torch.clamp(overlap_max - overlap_min, min=0)
-        overlap_volume = torch.prod(overlap_dims)
+    #     overlap_min = torch.max(min_corner1, min_corner2)
+    #     overlap_max = torch.min(max_corner1, max_corner2)
+    #     overlap_dims = torch.clamp(overlap_max - overlap_min, min=0)
+    #     overlap_volume = torch.prod(overlap_dims)
 
-        return overlap_volume
+    #     return overlap_volume
 # from Bio.PDB import PDBIO, Chain, Residue, Atom, Model, Structure
 
     def save_pdb(self, output_path):
@@ -257,15 +265,20 @@ def optimize_and_plot(n, epochs, input_monomer_pdb, output_dir, device):
 
     for epoch in range(epochs):
         # 初めは lr を大きくしておく
-        if epoch == 100:
+        if min(epoch == 100 * n, epoch * 0.7):
             for param_group in optimizer.param_groups:
-                param_group['lr'] = 0.1
+                param_group['lr'] = 1
         elif epoch == 800:
             for param_group in optimizer.param_groups:
-                param_group['lr'] = 0.01
+                param_group['lr'] = 0.1
+        
         optimizer.zero_grad()
         loss = model()
         loss.backward()
+        # 確率的勾配を加える
+        if epoch % 10 == 0:
+            for param in model.parameters():
+                param.grad += 0.01 * torch.randn_like(param)
         optimizer.step()
         losses.append(loss.item())
         print(f"N: {n}, Epoch {epoch + 1}/{epochs}, Loss: {loss.item()}")
@@ -288,9 +301,10 @@ def optimize_and_plot(n, epochs, input_monomer_pdb, output_dir, device):
 n_values = range(2, 11)
 epochs = 1000
 # pjt_dir = "."
-pjt_dir = "/work/gs58/s58007/casp16_Dojo" # wisteria
+# pjt_dir = "/work/gs58/s58007/casp16_Dojo" # wisteria
+pjt_dir = "/large/otgk/casp/casp16" # mdx
 input_monomer_pdb = pjt_dir + "/R0254/pdb/S_000144.pdb"
-output_dir = pjt_dir + "/R0254/pdb/multimer"
+output_dir = pjt_dir + "/R0254/pdb/multimer2"
 
 # Device setup
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
